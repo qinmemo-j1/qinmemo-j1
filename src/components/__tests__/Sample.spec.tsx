bc07a113ee58e4988ff35e59b94f6cfe6c459ba0
kosu <kosumon27@gmail.com>
2022-02-25 15:32:25 +0900

    🔧 test追加

---
+++ src/components/__tests__/Sample.spec.tsx
@@ -0,0 +1,11 @@
+// import { Sample } from "@/components/Sample";
+import { render } from "@testing-library/react";
+import { Sample } from "../Sample";
+
+describe("Sampleコンポーネント", () => {
+  test("should first", () => {
+    const { getByText } = render(<Sample />);
+    expect(getByText("Nextjs+Jestのサンプルサプリ")).toBeTruthy();
+    expect(getByText("設定がすごく楽になりました。")).toBeTruthy();
+  });
+});
